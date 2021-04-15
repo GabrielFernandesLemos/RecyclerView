9a7452dc789ba61d39d73d4868be305a8cac37c3
gabriel_lemos03@hotmail.com <yecgaa48252>
2021-04-15 17:35:04 -0300

    Cria Activity ContactDetail.kt

--- app/src/main/java/com/devlemos/recyclerview/ContactDetail.kt
+++ app/src/main/java/com/devlemos/recyclerview/ContactDetail.kt
@@ -2,10 +2,44 @@ package com.devlemos.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import com.devlemos.recyclerview.model.Contact
 
 class ContactDetail : AppCompatActivity() {
+
+    private  var contact : Contact? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_detail)
+
+        getExtras()
+        bindViews()
+        initToolBar()
+    }
+
+    private fun initToolBar(){
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+
+    private fun getExtras(){
+        contact = intent.getParcelableExtra(EXTRA_CONTACT)
+    }
+
+    private fun bindViews(){
+        findViewById<TextView>(R.id.txtDatailName).text = contact?.name
+        findViewById<TextView>(R.id.txtDetailNumber).text = contact?.number
+    }
+
+    companion object{
+        const val EXTRA_CONTACT: String = "EXTRA_CONTACT"
     }
 }
