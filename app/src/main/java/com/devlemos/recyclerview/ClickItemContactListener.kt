494a7fbc199d2f31fb42c71331009b3311455a63
gabriel_lemos03@hotmail.com <yecgaa48252>
2021-04-15 17:36:20 -0300

    Cria interface ClickItemContactListener.kt

---
+++ app/src/main/java/com/devlemos/recyclerview/ClickItemContactListener.kt
@@ -0,0 +1,7 @@
+package com.devlemos.recyclerview
+
+import com.devlemos.recyclerview.model.Contact
+
+interface ClickItemContactListener {
+    fun clickItemContact(contact : Contact)
+}
--- app/src/main/java/com/devlemos/recyclerview/adapter/ContactAdapter.kt
+++ app/src/main/java/com/devlemos/recyclerview/adapter/ContactAdapter.kt
@@ -5,18 +5,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.devlemos.recyclerview.ClickItemContactListener
 import com.devlemos.recyclerview.R
 import com.devlemos.recyclerview.model.Contact
 
-class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
+class ContactAdapter(var listener : ClickItemContactListener) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
 
     private val list: MutableList<Contact> = mutableListOf()
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.contact_item, parent, false)
-        return ContactViewHolder(view)
+        return ContactViewHolder(view, list, listener)
     }
 
     override fun getItemCount(): Int = list.size
@@ -31,9 +31,16 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() 
         notifyDataSetChanged()
     }
 
-    class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ContactViewHolder(itemView: View, var list: List<Contact>, var listener: ClickItemContactListener) : RecyclerView.ViewHolder(itemView) {
         private val txtName: TextView = itemView.findViewById(R.id.txtName)
         private val txtNumber: TextView = itemView.findViewById(R.id.txtNumber)
+
+        init {
+            itemView.setOnClickListener {
+                listener.clickItemContact(list[adapterPosition])
+            }
+        }
+
         fun bind(contact: Contact) {
             txtName.text = contact.name
             txtNumber.text = contact.number
